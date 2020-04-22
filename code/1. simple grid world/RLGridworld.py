7be687a1be1efe27f718c2e2d31d8085e9909efe
UnknownPgr <unknownpgr@gmail.com>
2020-04-22 11:42:47 +0900

    Add RL gridworld

---
+++ code/1. simple grid world/RLGridworld.py
@@ -0,0 +1,97 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+
+W = 50
+H = 50
+
+
+class AQ_Function:
+    def __init__(self):
+        self.table = np.zeros([H, W])
+
+    def calc(self, state):
+        x = state[0]
+        y = state[1]
+        table = self.table
+        # NESW
+        return [
+            table[(y-1) % H, x],
+            table[y, (x+1) % W],
+            table[(y+1) % H, x],
+            table[y, (x-1) % W]
+        ]
+
+    def update(self, befState, newValue):
+        self.table[befState[1], befState[0]] = newValue
+
+
+def selectOne(values):
+    M = -99999
+    r = []
+    for i in range(len(values)):
+        v = values[i]
+        if v > M:
+            M = v
+            r = [i]
+        if v == M:
+            r.append(i)
+    if len(r) == 1:
+        return r[0]
+    return r[random.randint(0, len(r)-1)]
+
+
+class AQ_Agent:
+    def __init__(self, function):
+        self.function = function
+        self.state = [0, 0]
+
+    def move(self, dest):
+        action = selectOne(self.function.calc(self.state))
+        befState = self.state
+        if action == 0:
+            self.state[1] -= 1
+        elif action == 1:
+            self.state[0] += 1
+        elif action == 2:
+            self.state[1] -= 1
+        elif action == 3:
+            self.state[0] -= 1
+        self.state[0] %= W
+        self.state[1] %= H
+        newValue = np.max(self.function.calc(self.state))-1
+        if self.state[0] == dest[0] and self.state[1] == dest[1]:
+            self.function.update(befState, -1)
+            return True
+        self.function.update(befState, newValue)
+        return False
+
+
+func = AQ_Function()
+agent = AQ_Agent(func)
+
+print(agent.state)
+for i in range(50000):
+    if agent.move([W-3, H-3]):
+        agent.state = [random.randint(0, W-1), random.randint(0, H-1)]
+
+
+fmap = agent.function.table
+
+with open('r.csv', 'w') as o:
+    for row in fmap:
+        for value in row:
+            o.write(f'{value},')
+        o.write('\n')
+
+
+X = np.arange(0, W, 1)
+Y = np.arange(0, H, 1)
+X, Y = np.meshgrid(X, Y)
+Z = fmap
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X, Y, Z, linewidth=0, antialiased=False)
+plt.show()
